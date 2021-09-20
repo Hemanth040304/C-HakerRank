33c71b2e1527812467b41bc70be580a84d811b6c
Hemanth040304 <91045353+Hemanth040304@users.noreply.github.com>
2021-09-20 22:06:40 +0530

    Add files via upload

---
+++ Sum and Difference of Two Numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int number1,number2;
+    float number3,number4;
+    number3*1.0,number4*1.0;
+    scanf("%d %d %f %f",&number1,&number2,&number3,&number4);
+    printf("%d %d\n%.1f %.1f",number1+number2,number1-number2,number3+number4,number3-number4);
+}
+
